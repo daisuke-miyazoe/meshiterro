41a2039dfb34cf191ea26b81f907c1312db042bb
daisuke miyazoe <hndfavodaismi@gmail.com>
2020-09-30 22:17:25 +0900

    meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '805300c498476dfb9a4015b1186f9e017166515e63791d49c328928fb7f00166cc4c95e06dd8fd6269dc50838dd58e277352df01bb283e1092c73152a9c5de23'
+Refile.secret_key = '1116374ba5bd3806ed7a7b80ec980638617e0841dca0b5f9b0f9c3417ea16ffa1f0f4bb559403224ea79a4cd5345580ce7929092f6ee13bb3256835fb485798d'
